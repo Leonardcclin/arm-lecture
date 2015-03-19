97e328f5b7d1b1f03a80b70e26f35a921a3ba0c1
Leonard <Leonard.cclin@gmail.com>
2015-03-19 13:51:27 +0800

    Lab#38 Non-recursive fabonacci
    It's non-recursive version fabonacci sequence.

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -9,35 +9,22 @@
 	.type fibonacci, function
 
 fibonacci:
-	@ ADD/MODIFY CODE BELOW
-	@ PROLOG
-	push {r3, r4, r5, lr}
+	push {r3,r4,r5,r6,lr}	@function prolog
+	add r4, r0, #0		@r4<-arg0
+	mov r5, #0		@r5<-#0
+	sub r5, r5, #1		@r5<-#-1 previous
+	mov r6, #1		@r6<-#1 result
+.loop:
+	add r3, r5, r6
+	mov r5, r6
+	mov r6, r3
 
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
+	sub r4, r4, #1		@r4--
+	cmp r4, #-1		@compare with -1
+	bne .loop		@branch if not equal
+	mov r0, r6		@return result
 
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
-
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
-
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
-
-	@ R0 = R5 + R0 (update flags)
-
-	pop {r3, r4, r5, pc}		@EPILOG
-
-	@ END CODE MODIFICATION
-.L3:
-	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
-
-.L4:
-	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
+	pop {r3,r4,r5,r6,pc}
 
 	.size fibonacci, .-fibonacci
 	.end
